46949d33cbbca210c72f6367b87df94bdc209d7b
Edgard3033 <ed233094@outlook.com>
2023-05-23 16:15:43 -0400

    made all functions that aren't complete

--- db/adapters/routines.js
+++ db/adapters/routines.js
@@ -1,1 +1,19 @@
 const client = require("../client");
+
+async function createRoutines({ name, goal }) {
+  try {
+    console.log("starting to insert ROUTINES into db ");
+    const {
+      rows: [routine],
+    } = await client.query(`
+    
+    `);
+    return routine;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  createRoutines,
+};
