ea84da74ff9a74b23a717e8ab84171497da1c91f
Edgard3033 <ed233094@outlook.com>
2023-05-24 11:45:59 -0400

    Completed db/seed

--- db/adapters/routines.js
+++ db/adapters/routines.js
@@ -5,15 +5,43 @@ async function createRoutines({ name, goal }) {
     console.log("starting to insert ROUTINES into db ");
     const {
       rows: [routine],
-    } = await client.query(`
-    
-    `);
+    } = await client.query(
+      `
+    INSERT INTO routines(name, goal)
+    VALUES ($1, $2)
+    RETURNING *;
+    `,
+      [name, goal]
+    );
     return routine;
   } catch (error) {
     throw error;
   }
 }
 
+async function getRoutineById(id) {
+  try {
+    console.log("Getting a routine by id...");
+    const {
+      rows: [routine],
+    } = await client.query(
+      `
+    SELECT *
+    FROM routines
+    JOIN activities ON routine.id = activities.routine_id
+    WHERE routines.id = $1
+    `,
+      [id]
+    );
+    return routine;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getRoutinesWithoutActivities() {}
+
 module.exports = {
   createRoutines,
+  getRoutineById,
 };
