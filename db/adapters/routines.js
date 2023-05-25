83875cf108678c36da40b72e612d8a36d320862c
Edgard3033 <ed233094@outlook.com>
2023-05-24 20:52:48 -0400

    routines were deleted for unknown reason

--- db/adapters/routines.js
+++ db/adapters/routines.js
@@ -5,8 +5,64 @@ async function createRoutines({ name, goal }) {
     console.log("starting to insert ROUTINES into db ");
     const {
       rows: [routine],
+    } = await client.query(
+      `
+    INSERT INTO routines(name, goal)
+    VALUES ($1, $2)
+    RETURNING *;
+    `,
+      [name, goal]
+    );
+    return routine;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getRoutineById(id) {
+  try {
+    console.log("Getting a routine by id...");
+    const {
+      rows: [routine],
+    } = await client.query(
+      `
+    SELECT *
+    FROM routines
+    JOIN activities ON routine.id = activities.routine_id
+    WHERE routines.id = $1;
+    `,
+      [id]
+    );
+    return routine;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getRoutinesWithoutActivities() {
+  try {
+    console.log("...getting routines without activities");
+    const {
+      rows: [routine],
     } = await client.query(`
-    
+  SELECT *
+  FROM routines;
+  `);
+    return routine;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllRoutines() {
+  try {
+    console.log("..getting all routines");
+    const {
+      rows: [routine],
+    } = await client.query(`
+    SELECT *
+    FROM routines
+    JOIN activities ON routines;
     `);
     return routine;
   } catch (error) {
@@ -14,6 +70,15 @@ async function createRoutines({ name, goal }) {
   }
 }
 
+async function getAllPublicRoutines() {
+  try {
+    console.log("...getting all public routines");
+  } catch (error) {}
+}
+
 module.exports = {
   createRoutines,
+  getRoutineById,
+  getRoutinesWithoutActivities,
+  getAllRoutines,
 };
