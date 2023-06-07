bab0c93287668aaee56694394b98e930b84d73e4
Edgard3033 <ed233094@outlook.com>
2023-06-07 11:42:07 -0400

    Made auth provider

---
+++ client/src/components/auth/AuthProvider.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState, useEffect } from "react";
+// import { fetchMe } from "../../api/auth";
+
+export const AuthContext = createContext();
+
+const AuthProvder = ({ children }) => {
+  const [user, setUser] = useState({ username: "Guest" });
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    async function getMe() {
+      try {
+        const { user } = await fetchMe();
+        setUser(user);
+      } catch (error) {
+        setUser({ username: "Guest" });
+      }
+    }
+    getMe();
+  }, [loggedIn]);
+
+  const contextValue = {
+    user,
+    setUser,
+    loggedIn,
+    setLoggedIn,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvder;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import AuthProvder from "./components/auth/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvder>
+        <App />
+      </AuthProvder>
     </BrowserRouter>
   </React.StrictMode>
 );
